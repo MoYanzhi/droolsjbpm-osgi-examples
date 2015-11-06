848f14ab1cc0ba73baec67bcd02913de51457f2d
Charles Moulliard <ch007m@gmail.com>
2015-11-06 19:17:03 +0100

    Update example to use Fire Rule

---
+++ simple-injection-ds-stateful/src/main/java/org/drools/example/osgi/FireStatefulOsgiDeclarativeService.java
@@ -0,0 +1,79 @@
+package org.drools.example.osgi;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.drools.example.model.Fire;
+import org.drools.example.model.Room;
+import org.drools.example.model.Sprinkler;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+import org.osgi.framework.BundleContext;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+@Component(name = "org.osgi.framework.BundleActivator")
+public class FireStatefulOsgiDeclarativeService {
+
+    private KieSession ksession;
+
+    @Activate
+    public void start(final BundleContext bc) throws Exception {
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kcont = ks.newKieClasspathContainer(getClass().getClassLoader());
+        KieBase kbase = kcont.getKieBase("sampleKBase");
+
+        this.ksession = kbase.newKieSession();
+        System.out.println("Kie Stateful Session created.");
+
+        String[] names = new String[]{"kitchen", "bedroom", "office", "livingroom"};
+        Map<String,Room> name2room = new HashMap<String,Room>();
+
+        for( String name: names ){
+            Room room = new Room( name );
+            name2room.put( name, room );
+            ksession.insert( room );
+            Sprinkler sprinkler = new Sprinkler( room );
+            ksession.insert( sprinkler );
+        }
+
+        ksession.fireAllRules();
+        pause();
+
+        Fire kitchenFire = new Fire( name2room.get( "kitchen" ) );
+        Fire officeFire = new Fire( name2room.get( "office" ) );
+
+        FactHandle kitchenFireHandle = ksession.insert( kitchenFire );
+        FactHandle officeFireHandle = ksession.insert( officeFire );
+
+        ksession.fireAllRules();
+        pause();
+
+        ksession.delete( kitchenFireHandle );
+        ksession.delete(officeFireHandle);
+
+        ksession.fireAllRules();
+        
+    }
+
+    @Deactivate
+    public void stop(final BundleContext bc) throws Exception {
+        if (this.ksession != null) {
+            this.ksession.dispose();
+            System.out.println("KieSession disposed.");
+        }
+    }
+
+    public static void pause() {
+        System.out.println( "Pressure enter to contnue" );
+        Scanner keyboard = new Scanner(System.in);
+        keyboard.nextLine();
+    }
+
+}
